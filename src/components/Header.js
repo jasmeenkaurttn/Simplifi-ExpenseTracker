876f8a158980f54ba492567d9c2eb6953a9aa4f5
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2023-04-26 18:08:09 +0530

    UI fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
-import { Card, Group, Text } from '@mantine/core'
+import { Card, Group, Space, Text, createStyles } from '@mantine/core'
 import React from 'react'
+
+const useStyles = createStyles((theme) => ({
+  hover: {
+    cursor: 'pointer',
+  }
+}));
 
 function Header() {
   const user = JSON.parse(localStorage.getItem('user'))
+  const { classes } = useStyles();
+
   return (
     <div>
       <Card shadow='md' withBorder p={15}>
@@ -16,16 +24,14 @@ function Header() {
             SIMPLIFI
           </Text>
           <Group className='flex items-center'>
-            {user?.name}
-            <Text className='flex items-center cursor-pointer'><i className="ri-logout-box-r-line"
-              onClick={() => {
-                localStorage.removeItem("user");
-                window.location.reload();
-              }}
-            ></i><span className='cursor-pointer'>Logout</span></Text>
-            {/* <Text size='sm' variant='text' color='teal'>
-               Logout
-            </Text> */}
+            <Text color='cyan' fw={600}>{user?.name}</Text>
+            <Text className='flex items-center cursor-pointer' onClick={() => {
+                  localStorage.removeItem("user");
+                  window.location.reload();
+                }}>
+              <i className="ri-logout-box-r-line"></i>
+              <Space w="xs" />
+              <span className={`${classes.hover}`}>Logout</span></Text>
           </Group>
         </div>
       </Card>
